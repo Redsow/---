f35c3cdffd0ca3aa35995e9d5db48efe185f86f1
Redsow <efremov.ivan04-06-2004@yandex.ru>
2023-06-02 13:44:37 +0300

    first commit

---
+++ test/vite-project/src/lib/Component.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+
+let data = {};
+
+
+
+onMount(async () => {
+  try {
+    const response = await fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=a3bfbc120b2349b78a775666333a10e0`);
+    data = await response.json();
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+  }
+});
+  </script>
+  
+  <div>
+    {#if Object.keys(data).length > 0}
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+    {:else}
+      <p>Загрузка данных...</p>
+    {/if}
+  </div>
